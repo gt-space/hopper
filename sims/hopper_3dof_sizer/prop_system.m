e61e4bdc8f1c3cc6594bad04de1ddb296e072e4c
Vedanth27 <vedanthsj27@gmail.com>
2026-01-18 20:13:05 -0500

    prop system model done, ready to integrate into sim

--- sims/hopper_3dof_sizer/prop_system.m
+++ sims/hopper_3dof_sizer/prop_system.m
@@ -1,7 +1,8 @@
 %% Hopper Propulsion System
 
-function [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR] = prop_system(thrust, ox_name, fu_name, ox_vap_P, ox_tank_P, fu_tank_P, ox_sys_CdA, fu_sys_CdA, eta_cstar, At, eps)
-
+function [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR] = prop_system(thrust_target, ox_name, fu_name, fu_temp, ox_vap_P, ox_tank_P, fu_tank_P, ox_sys_CdA, fu_sys_CdA, eta_cstar, At, eps)
+    
+    %pyenv
     %CP = py.importlib.import_module('CoolProp.CoolProp');
     
     % Hardcoded Pa - implement this as a function of altitude so it changes
@@ -12,6 +13,8 @@ function [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR] = prop_system(thrust, ox_n
     R = 8.314; % J/mol-K
     MW = 21.683 / 1000; % g/mol --> kg/mol
     
+    MR_target = 3;
+
     % Gamma set - implement CEA to calculate gamma!
     gamma = 1.26;
 
@@ -21,44 +24,51 @@ function [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR] = prop_system(thrust, ox_n
 
     ox_rho = 896; % kg/m^3
     fu_rho = 786; % kg/m^2
-    
-    % Equations -- x(1) = ox_mdot, x(2) = fu_mdot, x(3) = Pc, x(4) = tot_mdot
-    fun = @(x) [
-        x(1) - (ox_sys_CdA * sqrt(2 * ox_rho * (ox_tank_P - x(3))));
-        x(2) - (fu_sys_CdA * sqrt(2 * fu_rho * (fu_tank_P - x(3))));
-        x(4) - (x(1) + x(2));
-        cstar_act - ((x(3) * At) / x(4));
-    ];
-    x0 = [0.9, 0.3, 250*6894.7, 1.2]; % Initial Guess
-    x = fsolve(fun, x0);
-    ox_mdot  = x(1);
-    fu_mdot  = x(2);
-    Pc       = x(3);
-    tot_mdot = x(4);
 
-    MR = ox_mdot / fu_mdot; 
-   
-    
     % Solve Area-Mach Relation
     f = @(M) (eps)^2 - (1 / M^2) * (((2 / (gamma + 1)) * (1 + 0.5*(gamma - 1)*M^2))^((gamma + 1) / (gamma - 1)));
     M0 = 3;
     M = fzero(f, M0);
     
-    % Pe Solve
-    Pe = Pc / ((1 + 0.5*(gamma - 1)*M^2) ^ (gamma / (gamma - 1))); % Pa
+    % Equations -- x(1) = ox_mdot, x(2) = fu_mdot, x(3) = Pc, x(4) = tot_mdot
+    fun = @(x) [
+        x(7) - (sqrt(1 / ((1 / (ox_sys_CdA^2)) + (1 / (x(5)^2)))));
+        x(8) - (sqrt(1 / ((1 / (fu_sys_CdA^2)) + (1 / (x(6)^2)))));
+        x(1) - (x(7) * sqrt(2 * ox_rho * (ox_tank_P - x(3))));
+        x(2) - (x(8) * sqrt(2 * fu_rho * (fu_tank_P - x(3))));
+        x(4) - (x(1) + x(2));
+        cstar_act - ((x(3) * At) / x(4));
+        x(9) - (x(3) / ((1 + 0.5*(gamma - 1)*M^2) ^ (gamma / (gamma - 1))));
+        x(10) - (x(1) / x(2));
+        x(10) - MR_target;
+        x(11) - (gamma / (((1 / (cstar_act * ((0.5*(gamma + 1))^((-gamma - 1) / (2 * (gamma - 1))))))^2) * (R/MW))); % K 
+        x(12) - (sqrt(((2 * gamma) / (gamma - 1)) * (R / MW) * (x(11)) * (1 - ((x(9) / x(3))^((gamma - 1)/gamma))))); % m/s
+        x(13) - (x(4) * x(12) + At * eps * (x(9) - Pa));
+        x(13) - thrust_target;
+    ];
+    x0 = [0.9, 0.3, 250*6894.7, 1.2, 3.633E-5, 1.3144E-5, 1.8165E-5, 6.572E-6, 101325, 3, 2200, 2000, 1334]; % Initial Guess
+    options = optimoptions('fsolve', 'MaxFunctionEvaluations', 8000);
+    x = fsolve(fun, x0, options);
+    ox_mdot  = x(1);
+    fu_mdot  = x(2);
+    Pc       = x(3);
+    tot_mdot = x(4);
+    ox_valve_CdA = x(5);
+    fu_valve_CdA = x(6);
+    ox_tot_CdA = x(7);
+    fu_tot_CdA = x(8);
+    Pe = x(9);
+    MR = x(10);
+    T0 = x(11);
+    U_e = x(12);
+    thrust = x(13);
 
-    % Exit Velocity
-    A = (1 / (cstar_act * ((0.5*(gamma + 1))^((-gamma - 1) / (2 * (gamma - 1))))))^2;
-    T0 = gamma / (A * (R/MW)); % K 
-    U_e = sqrt(((2 * gamma) / (gamma - 1)) * (R / MW) * (T0) * (1 - ((Pe / Pc)^((gamma - 1)/gamma)))); % m/s
     % import CEA tables - output all gas properties
     
-    momentum_thrust = tot_mdot * U_e;
-    pressure_thrust = At * eps * (Pe - Pa);
-    thrust = momentum_thrust + pressure_thrust;
     % vary Pc to solve for thrust - keep constant MR
 
     % Assign - OX Mdot, FU mdot, Total Mdot, Pc
+    % - vary the 
     
 
 end
---
+++ sims/hopper_3dof_sizer/prop_system_testing.m
@@ -0,0 +1,25 @@
+% Function Testing
+thrust_target = 400 * 4.44822; % lbf --> N
+ox_name = "N2O";
+fu_name = "Ethanol";
+fu_temp = 293; % K
+ox_vap_P = 200 * 6894.76; % psia --> Pa
+ox_tank_P = 500 * 6894.76; % psia --> Pa
+fu_tank_P = 500 * 6894.76; % psia --> Pa
+ox_sys_CdA = 2.0975E-5; % m^2
+fu_sys_CdA = 7.588E-6; % m^2
+eta_cstar = 0.85;
+At = 1.150 * 0.00064516; % in^2 --> m^2
+eps = 3.356;
+
+[ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR] = prop_system(thrust_target, ox_name, fu_name, fu_temp, ox_vap_P, ox_tank_P, fu_tank_P, ox_sys_CdA, fu_sys_CdA, eta_cstar, At, eps);
+
+Pc = Pc / 6895.76; % Pa --> psia
+
+% Prints
+fprintf('OX Mdot : %.4f kg/s\n', ox_mdot)
+fprintf('FU Mdot : %.4f kg/s\n', fu_mdot)
+fprintf('Total Mdot : %.4f kg/s\n', tot_mdot)
+fprintf('Pc : %.3f psia\n', Pc)
+fprintf('Thrust : %.2f lbf\n', thrust / 4.44822)
+fprintf('MR : %.2f\n', MR)
