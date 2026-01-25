8efe88b102ce3eff8d5367a1a66ecbf0f0433b55
Aniket Singh <asingh3218@gatech.edu>
2026-01-24 19:35:17 -0500

    kerolox lookup

--- sims/hopper_3dof_sizer/load_lookup.m
+++ sims/hopper_3dof_sizer/load_lookup.m
@@ -1,4 +1,5 @@
 data = readmatrix("mdot_lookup.xlsx");
 
 bp = data(:,1);   % breakpoints
-tbl = data(:,2);
+tbl1 = data(:,2);
+tbl2 = data(:,3);
--- sims/hopper_3dof_sizer/look_up_table_generation.m
+++ sims/hopper_3dof_sizer/look_up_table_generation.m
@@ -1,22 +1,23 @@
 thrust_target = 200:540 * 4.44822; % lbf --> N
-ox_name = "N2O";
-fu_name = "Ethanol";
+ox_name = "O2";
+fu_name = "Dodecane";
 fu_temp = 293; % K
-ox_vap_P = 500 * 6894.76; % psia --> Pa
+ox_temp = 90; % K
 ox_tank_P = 500 * 6894.76; % psia --> Pa
 fu_tank_P = 500 * 6894.76; % psia --> Pa
-ox_sys_CdA = 2.0975E-5; % m^2
-fu_sys_CdA = 7.588E-6; % m^2
+ox_sys_CdA = 1.7827-5; % m^2
+fu_sys_CdA = 1.1E-5; % m^2
 eta_cstar = 0.85;
-At = 1.150 * 0.00064516; % in^2 --> m^2
-eps = 3.356;
+At = 1.138 * 0.00064516; % in^2 --> m^2
+eps = 3.655;
 
-mdot_data = cell(length(thrust_target),2);
+mdot_data = cell(length(thrust_target),3);
 
 for i = 1:length(thrust_target)
-    [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR, ox_valve_CdA, fu_valve_CdA] = prop_system(thrust_target(i), ox_name, fu_name, fu_temp, ox_vap_P, ox_tank_P, fu_tank_P, ox_sys_CdA, fu_sys_CdA, eta_cstar, At, eps);
+    [ox_mdot, fu_mdot, tot_mdot, Pc, thrust, MR, ox_valve_CdA, fu_valve_CdA] = prop_system(thrust_target(i), ox_name, fu_name, fu_temp, ox_temp, ox_tank_P, fu_tank_P, ox_sys_CdA, fu_sys_CdA, eta_cstar, At, eps);
     mdot_data{i,1} = thrust_target(i);
-    mdot_data{i,2} = tot_mdot;
+    mdot_data{i,2} = ox_mdot;
+    mdot_data{i,3} = fu_mdot;
 end
 
 writecell(mdot_data, 'mdot_lookup.xlsx')
