223ce2073e2666c0579dcd8f612911bf704ca0a8
Aniket Singh <asingh3218@gatech.edu>
2026-03-09 23:54:19 -0400

    Include mass change

--- sims/hopper_6dof_sizer/GenerateLQ.m
+++ sims/hopper_6dof_sizer/GenerateLQ.m
@@ -1,105 +1,69 @@
+clear
+clc
+close all
 
-%% ================= GENERATE TRAJECTORY =================
+%% GENERATE TRAJECTORY
 
-[Trajectory, unom, tgrid] = Trajectory3();
+[Trajectory,unom,tgrid,m_profile,I_profile] = Trajectory3();
 
-N  = length(tgrid);
+N = length(tgrid);
+
 nx = 13;
 nu = 4;
 
-%% ================= COST MATRICES =================
+%% COST MATRICES
 
-Sf = diag([ ...
-1 ...
-1 ...
-1e10 ...
-1 ...
-1 ...
-1e6 ...
-1 ...
-1 ...
-1 ...
-1000 ...
-1000 ...
-1000 ...
-1000 ]);
+Sf = diag([1 1 1e10 1 1 1e6 1 1 1 1000 1000 1000 1000]);
+Q  = diag([1 1 1e10 1 1 10 1 1 1 100 100 100 100]);
+R  = diag([100 1000 1000 1000]);
 
-Q = diag([ ...
-1 ...
-1 ...
-1e8 ...
-1 ...
-1 ...
-10 ...
-1 ...
-1 ...
-1 ...
-100 ...
-100 ...
-100 ...
-100 ]);
-
-R = diag([ ...
-2e6 ...
-1000 ...
-1000 ...
-1000 ]);
-
-%% ================= VEHICLE PARAMETERS =================
+%% PARAMETERS
 
 params = struct();
 
-params.g = 9.80665;
-params.m = 114;
-
-params.d   = 1.0;
+params.g = 9.81;
+params.d = 1.0;
 params.r_m = 0.125;
 
-Ix = 16.5;
-Iy = 16.5;
-Iz = 16.5;
+%% PREALLOCATE
 
-params.I = diag([Ix Iy Iz]);
+A=zeros(nx,nx,N);
+B=zeros(nx,nu,N);
+Fref=zeros(N,nx);
 
-%% ================= LINEARIZE ALONG TRAJECTORY =================
+%% LINEARIZATION LOOP
 
-A = zeros(nx,nx,N);
-B = zeros(nx,nu,N);
-Fref = zeros(N,nx);
+for i=1:N
 
-for i = 1:N
+    params.m = m_profile(i);
+    params.I = I_profile(:,:,i);
 
     x = Trajectory(i,:)';
     u = unom(i,:)';
 
-    [Ai,Bi,f0] = HopperLinearization_6DOF(x,u,params);
+    [Ai,Bi,f0] = HopperLinearization_6DOF_inertial(x,u,params);
+    
 
-    A(:,:,i) = Ai;
-    B(:,:,i) = Bi;
-    Fref(i,:) = f0';
+    A(:,:,i)=Ai;
+    B(:,:,i)=Bi;
+    Fref(i,:)=f0';
 
 end
 
-%% ================= COMPUTE TVLQR GAINS =================
+%% COMPUTE LQR
 
 GainTable = TrajectoryFollowingGains2( ...
-    Sf,Q,R,Trajectory,A,B,tgrid,unom,params,Fref);
+Sf,Q,R,Trajectory,A,B,tgrid,unom,params,Fref);
 
 K1 = GainTable.K1;
-K2 = GainTable.K2';
+K2grid = GainTable.K2';
 
-%% ================= FLATTEN FOR SIMULINK =================
+%% FLATTEN K1 FOR SIMULINK
 
-K1flat = zeros(N,52);
+K1flat=zeros(N,52);
 
-for i = 1:N
+for i=1:N
     K1flat(i,:) = reshape(K1(:,:,i).',1,52);
 end
 
-%% ================= EXPORT TIMESERIES =================
-
-z_ref_ts  = timeseries(Trajectory(:,3),tgrid);
-vz_ref_ts = timeseries(Trajectory(:,6),tgrid);
-T_ref_ts  = timeseries(unom(:,1),tgrid);
-
-disp("TVLQR Gain Generation Complete")
+disp("LQR Gains Generated")
