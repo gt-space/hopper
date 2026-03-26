a008a5da8cb37ee06f67164b6cdad6d9425ca147
tdeodhar3 <mail.tejas.deodhar@gmail.com>
2026-03-26 15:31:30 -0400

    Add files via upload
    bandwidth calculator

---
+++ sims/hopper_6dof_sizer/bandwidth_attempt1_throttle_valve.m
@@ -0,0 +1,18 @@
+out = sim('throttle_valve_simulink');
+
+Pout = out.Pout_log.signals.values;
+t    = out.Pout_log.time;
+
+Pout = Pout(:);
+t    = t(:);
+
+info_outer = stepinfo(Pout, t, 100);
+%                     ^signal       ^time     ^final expected value (your P_set = 100)
+info_inner = stepinfo(out.theta_log, out.tout, 49.6758967952748);
+%                     ^signal        ^time     ^final expected value (max theta = 90°)
+
+BW_outer = 0.35 / info_outer.RiseTime;
+BW_inner = 0.35 / info_inner.RiseTime;
+fprintf('Outer BW: %.4f rad/s\n', BW_outer);
+fprintf('Inner BW: %.4f rad/s\n', BW_inner);
+fprintf('Ratio (inner/outer): %.11fx -- target is 5-10x\n', BW_inner / BW_outer);
